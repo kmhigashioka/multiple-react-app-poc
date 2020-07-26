dea11b24b1d817ad07fae57405292363bb091712
Kazuhito Higashioka <kmhigashioka@gmail.com>
2020-07-26 15:23:40 +0800

    Render app1, app2

--- packages/base/src/App.js
+++ packages/base/src/App.js
@@ -1,26 +1,78 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  useScript('http://localhost:3001/static/js/bundle.js');
+  useScript('http://localhost:3001/static/js/0.chunk.js');
+  useScript('http://localhost:3001/static/js/main.chunk.js');
+  useScript('http://localhost:3002/static/js/bundle.js');
+  useScript('http://localhost:3002/static/js/0.chunk.js');
+  useScript('http://localhost:3002/static/js/main.chunk.js');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <nav className="Nav">
+          <div id="root-app1"></div>
+          <div id="root-app2"></div>
+        </nav>
+      </div>
+    </>
   );
 }
 
 export default App;
+
+let cachedScripts = [];
+function useScript(src) {
+  const [state, setState] = React.useState({
+    loaded: false,
+    error: false
+  });
+
+  React.useEffect(
+    () => {
+      if (cachedScripts.includes(src)) {
+        setState({
+          loaded: true,
+          error: false
+        });
+      } else {
+        cachedScripts.push(src);
+        let script = document.createElement('script');
+        script.src = src;
+        script.async = true;
+
+        const onScriptLoad = () => {
+          setState({
+            loaded: true,
+            error: false
+          });
+        };
+
+        const onScriptError = () => {
+          const index = cachedScripts.indexOf(src);
+          if (index >= 0) cachedScripts.splice(index, 1);
+          script.remove();
+          setState({
+            loaded: true,
+            error: true
+          });
+        };
+
+        script.addEventListener('load', onScriptLoad);
+        script.addEventListener('error', onScriptError);
+
+        document.body.appendChild(script);
+
+        return () => {
+          script.removeEventListener('load', onScriptLoad);
+          script.removeEventListener('error', onScriptError);
+        };
+      }
+    },
+    [src],
+  );
+
+  return [state.loaded, state.error];
+}
