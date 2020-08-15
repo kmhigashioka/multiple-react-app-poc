97a3f0c9700e7183167865c8936c1fb0fa01a415
Kazuhito Higashioka <kmhigashioka@gmail.com>
2020-08-16 01:57:52 +0800

    [app2] integrate to base

--- packages/app2/src/index.tsx
+++ packages/app2/src/index.tsx
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
@@ -7,21 +8,30 @@ import Language from './containers/Language';
 import * as serviceWorker from './serviceWorker';
 import { translationMessages } from './containers/Language/i18n';
 
-ReactDOM.render(
-  <Router>
-    <Language messages={translationMessages}>
-      <HelmetProvider>
-        <Helmet>
-          <meta name="description" content="" />
-        </Helmet>
-        <App />
-      </HelmetProvider>
-    </Language>
-  </Router>,
-  document.getElementById('root'),
-);
+declare global {
+  interface Window {
+    renderApp2: any;
+    unmountApp1: any;
+  }
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+window.renderApp2 = (containerId, history) => {
+  ReactDOM.render(
+    <Router history={history}>
+      <Language messages={translationMessages}>
+        <HelmetProvider>
+          <Helmet>
+            <meta name="description" content="" />
+          </Helmet>
+          <App />
+        </HelmetProvider>
+      </Language>
+    </Router>,
+    document.getElementById(containerId),
+  );
+  serviceWorker.unregister();
+};
+
+window.unmountApp1 = containerId => {
+  ReactDOM.unmountComponentAtNode(document.getElementById(containerId));
+};
--- packages/base/src/AppNavigation.jsx
+++ packages/base/src/AppNavigation.jsx
@@ -9,6 +9,9 @@ const AppNavigation = () => (
       <li>
         <Link to="/app1">App 1</Link>
       </li>
+      <li>
+        <Link to="/app2">App 2</Link>
+      </li>
     </ul>
   </nav>
 );
